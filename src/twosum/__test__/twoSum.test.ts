1f1b0776ce6efbb55623004dec0271a525b9d370
dewei.ma <dewei.ma@lvyuetravel.com>
2021-01-28 16:23:26 +0800

    chore: twosum test tips

--- src/twosum/__test__/twoSum.test.ts
+++ src/twosum/__test__/twoSum.test.ts
@@ -7,16 +7,16 @@
 import twoSum from '../twoSum';
 
 describe('two sum', ()=>{
-    it('return result', ()=>{
-        expect(twoSum([2,7,11,15], 9)).toEqual([0,1])
-    })
-    it('return result', ()=>{
-        expect(twoSum([3,2,4], 6)).toEqual([1,2])
-    })
-    it('return result', ()=>{
-        expect(twoSum([3,3], 6)).toEqual([0,1])
-    })
-    it('return result', ()=>{
-        expect(twoSum([3,3], 7)).toEqual([])
-    })
+    it('[2,7,11,15]/9:', ()=>{
+        expect(twoSum([2,7,11,15], 9)).toEqual([0,1]);
+    });
+    it('[3,2,4]/6:', ()=>{
+        expect(twoSum([3,2,4], 6)).toEqual([1,2]);
+    });
+    it('[3,3]/6:', ()=>{
+        expect(twoSum([3,3], 6)).toEqual([0,1]);
+    });
+    it('[3,3]/7:', ()=>{
+        expect(twoSum([3,3], 7)).toEqual([]);
+    });
 })
